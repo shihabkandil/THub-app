3f37fe4787e489cd513518fde52772cfb97fe0a7
Shihab Kandil <jupitar.orbit01@gmail.com>
2022-01-24 00:04:02 +0200

    movie model

---
+++ lib/data/models/movie.dart
@@ -0,0 +1,26 @@
+
+
+
+
+class Movie{
+  late String movieID;
+  late String title;
+  late String overview;
+  late String posterImgPath;
+  late String releaseDate;
+
+  Movie(this.movieID,
+      this.title,
+      this.overview,
+      this.posterImgPath,
+      this.releaseDate);
+
+  Movie.fromJson(Map<String,dynamic>json){
+    movieID = json["id"];
+    title = json["title"];
+    overview = json["overview"];
+    posterImgPath = json["poster_path"];
+    releaseDate = json["release_date"];
+  }
+
+}
